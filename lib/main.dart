dc9730c02291d41556f238a8c016f71dd13f340a
kishanhitk <kishansharma1231@gmail.com>
2020-06-23 16:02:40 +0530

    Added retrieve user from shared preference on launch.

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,47 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
 import 'package:jumsRebootFlutter/loginPage.dart';
+import 'package:jumsRebootFlutter/models/semsterButtons.dart';
+import 'package:jumsRebootFlutter/profilePage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+import 'models/user.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var uname = prefs.getString('uname');
+  var pass;
+  User user;
+  if (uname != null) {
+    pass = prefs.getString('pass');
+    String name = prefs.getString('name');
+    String course = prefs.getString('course');
+    String imgUrl = prefs.getString('imgUrl');
+    List<String> notices = prefs.getStringList('notices');
+    List<SemButtons> butons =
+        SemButtons.decodeButtons(prefs.getString('buttons'));
+    user = User(
+        notices: notices,
+        name: name,
+        course: course,
+        imgUrl: imgUrl,
+        buttons: butons);
+  }
+  print(uname);
+  runApp(MyApp(
+      isLoggedIn: uname != null ? true : false,
+      user: user,
+      uname: uname,
+      pass: pass));
 }
 
 class MyApp extends StatelessWidget {
+  bool isLoggedIn = false;
+  User user;
+  String uname;
+  String pass;
+  MyApp({this.isLoggedIn, this.user, this.pass, this.uname});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +60,13 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: isLoggedIn
+          ? ProfilePage(
+              user: user,
+              uname: uname,
+              pass: pass,
+            )
+          : LoginPage(),
     );
   }
 }
