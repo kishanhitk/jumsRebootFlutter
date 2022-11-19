fcbdabedde7f5b0583ac3c8964ff24b564a29460
kishanhitk <kishansharma1231@gmail.com>
2022-11-19 18:10:23 +0530

    chore: migrate to v2 embedding

--- android/app/src/main/kotlin/com/kishans/jumsRebootFlutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/kishans/jumsRebootFlutter/MainActivity.kt
@@ -2,7 +2,6 @@ package com.kishans.jumsRebootFlutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-import android.os.Build
 import android.view.ViewTreeObserver
 import android.view.WindowManager
 class MainActivity: FlutterActivity() {
